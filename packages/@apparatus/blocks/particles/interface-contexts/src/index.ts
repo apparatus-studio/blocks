2dc04174aa8793b2d0a6555e006371dc74421b11
Fernando Via Canel <fernando.via@gmail.com>
2020-05-10 12:30:58 +0200

    🌱 apparatus/blocks-particles-interface-contexts: add success and error interface contexts

--- packages/@apparatus/blocks/particles/interface-contexts/src/index.ts
+++ packages/@apparatus/blocks/particles/interface-contexts/src/index.ts
@@ -3,3 +3,5 @@ export type TInterfaceContexts = symbol & { type: 'InterfaceContexts' }
 export const INTERFACE_CONTEXT_REGULAR = Symbol('INTERFACE_CONTEXT_REGULAR') as TInterfaceContexts
 export const INTERFACE_CONTEXT_ACCENT = Symbol('INTERFACE_CONTEXT_ACCENT') as TInterfaceContexts
 export const INTERFACE_CONTEXT_MIDTONE = Symbol('INTERFACE_CONTEXT_MIDTONE') as TInterfaceContexts
+export const INTERFACE_CONTEXT_SUCCESS = Symbol('INTERFACE_CONTEXT_SUCCESS') as TInterfaceContexts
+export const INTERFACE_CONTEXT_ERROR = Symbol('INTERFACE_CONTEXT_ERROR') as TInterfaceContexts
