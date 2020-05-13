8083fe2dfc7f8968c71c198a8b71e7f487146609
Fernando Via Canel <fernando.via@gmail.com>
2020-05-13 19:31:18 +0200

    🐞 apparatus/blocks-atoms-footprint: cleanup

--- packages/@apparatus/blocks/atoms/footprint/src/index.tsx
+++ packages/@apparatus/blocks/atoms/footprint/src/index.tsx
@@ -1,10 +1,8 @@
 import React, { FC, ReactNode } from 'react'
-import { startWithType, mapContext, component } from 'refun'
 import { View } from '@primitives/view'
 import { TThemeableSpacer } from '@themeables/spacer'
 import { TTextLevel } from '@apparatus/blocks-particles-text-levels'
 import { createThemeableSpacer } from '@apparatus/blocks-contexts-theme'
-import { ContextInterface } from '@apparatus/blocks-contexts-interface'
 
 export type TAtomFootprint = {
   level: TTextLevel,
@@ -32,14 +30,9 @@ const Footprint: FC<TFootprint> = ({ blockStart, blockEnd, children }) => (
   </View>
 )
 
-export const AtomFootprint = component(
-  startWithType<TAtomFootprint>(),
-  mapContext(ContextInterface)
-)(
-  createThemeableSpacer<TThemeableSpacer>(
-    'Footprint',
-    Footprint
-  )
+export const AtomFootprint = createThemeableSpacer<TThemeableSpacer>(
+  'Footprint',
+  Footprint
 )
 
 AtomFootprint.displayName = 'AtomFootprint'
