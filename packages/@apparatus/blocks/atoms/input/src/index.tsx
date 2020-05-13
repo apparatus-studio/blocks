4c2a428e9d03d16a78e5adf69a75b1c2233921bd
Fernando Via Canel <fernando.via@gmail.com>
2020-05-13 19:30:16 +0200

    🌱 apparatus/blocks-atoms-input: adopt new input

--- packages/@apparatus/blocks/atoms/input/src/index.tsx
+++ packages/@apparatus/blocks/atoms/input/src/index.tsx
@@ -1,4 +1,3 @@
-import React, { FC } from 'react'
 import { TMapFocused, startWithType, mapContext, component } from 'refun'
 import { Input } from '@primitives/input'
 import { TThemeableSpacer } from '@themeables/spacer'
@@ -12,52 +11,17 @@ export type TAtomInput = {
   hasIcon?: boolean,
 } & TMapFocused
 
-type TInner = TAtomInput
-  & TThemeableSpacer
-  & TThemeableText
-  & TMapFocused
-
-const Inner: FC<TInner> = ({
-  onBlur,
-  onFocus,
-  blockStart,
-  blockEnd,
-  inlineStart,
-  inlineEnd,
-  fontFamily,
-  fontSize,
-  fontWeight,
-  lineHeight,
-  color,
-  onChange,
-  value,
-}) => (
-  <Input
-    fontFamily={fontFamily}
-    fontSize={fontSize}
-    fontWeight={fontWeight}
-    lineHeight={lineHeight}
-    color={color}
-    paddingLeft={inlineStart}
-    onBlur={onBlur}
-    onFocus={onFocus}
-    paddingRight={inlineEnd}
-    paddingTop={blockStart}
-    paddingBottom={blockEnd}
-    onChange={onChange}
-    value={value}
-  />
-)
-
-export const AtomInput = component(
-  startWithType<TAtomInput>(),
-  mapContext(ContextInterface)
-)(createThemeableSpacer<TThemeableSpacer>(
+export const AtomInput = createThemeableSpacer<TThemeableSpacer>(
   'Spacer_Input',
-  createThemeableText<TThemeableText>(
-    'Text_Input',
-    Inner
+  component(
+    startWithType<TAtomInput>(),
+    mapContext(ContextInterface)
+  )(
+    createThemeableText<TThemeableText>(
+      'Text_Input',
+      Input
+    )
   )
-))
+)
 
 AtomInput.displayName = 'AtomInput'
