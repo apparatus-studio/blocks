2ee0dae5c75c172bb0da8a09233014c534eec0af
Fernando Via Canel <fernando.via@gmail.com>
2020-05-17 01:04:54 +0200

    🌱 apparatus/blocks-themes-default-mappings: allow partial theme overrides

--- packages/@apparatus/blocks/themes/default-mappings/src/index.ts
+++ packages/@apparatus/blocks/themes/default-mappings/src/index.ts
@@ -1,5 +1,5 @@
 import { TTheme } from '@apparatus/blocks-types-theme'
-import { TTokens } from '@apparatus/blocks-themes-default-tokens'
+import * as defaultTokens from '@apparatus/blocks-themes-default-tokens'
 import { themeBackground } from './themeBackground'
 import { themeBorder } from './themeBorder'
 import { themeFootprint } from './themeFootprint'
@@ -10,25 +10,33 @@ import { themeSpacer_Input } from './themeSpacer_Input'
 import { themeSpacer } from './themeSpacer'
 import { themeIcon } from './themeIcon'
 
-export const createTheme = (tokens: TTokens): TTheme => ({
-  background: {
-    Background: themeBackground(tokens),
-  },
-  border: {
-    Border: themeBorder(tokens),
-  },
-  image: {},
-  spacer: {
-    Container: themeContainer(tokens),
-    Footprint: themeFootprint(tokens),
-    Spacer: themeSpacer(tokens),
-    Spacer_Input: themeSpacer_Input(tokens),
-  },
-  text: {
-    Text: themeText(tokens),
-    Text_Input: themeText_Input(tokens),
-  },
-  vectorShape: {
-    Icon: themeIcon(tokens),
-  },
-})
+export type TTokenOverrides = Partial<defaultTokens.TTokens>
+export const createTheme = (tokenOverrides: TTokenOverrides): TTheme => {
+  const tokens = {
+    ...defaultTokens,
+    ...tokenOverrides,
+  }
+
+  return {
+    background: {
+      Background: themeBackground(tokens),
+    },
+    border: {
+      Border: themeBorder(tokens),
+    },
+    image: {},
+    spacer: {
+      Container: themeContainer(tokens),
+      Footprint: themeFootprint(tokens),
+      Spacer: themeSpacer(tokens),
+      Spacer_Input: themeSpacer_Input(tokens),
+    },
+    text: {
+      Text: themeText(tokens),
+      Text_Input: themeText_Input(tokens),
+    },
+    vectorShape: {
+      Icon: themeIcon(tokens),
+    },
+  }
+}
