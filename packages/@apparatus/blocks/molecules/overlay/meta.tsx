9f1ccfdf6e8fd3ba00e2f4b8a40c1554ee715cbb
Fernando Via Canel <fernando.via@gmail.com>
2020-05-21 22:35:47 +0200

    🧹 apparatus/blocks-molecules-overlay: test with action sheet (most common use case)

--- packages/@apparatus/blocks/molecules/overlay/meta.tsx
+++ packages/@apparatus/blocks/molecules/overlay/meta.tsx
@@ -5,6 +5,9 @@ import { TEXT_LEVEL_SECONDARY_TITLE } from '@apparatus/blocks-particles-text-lev
 import { ContextParentSizeProvider, TContextParentSize } from '@apparatus/blocks-contexts-parent-size'
 import { Bottom } from '@apparatus/blocks-molecules-bottom'
 import { FullCover } from '@apparatus/blocks-molecules-full-cover'
+import { AtomSpacer } from '@apparatus/blocks-atoms-spacer'
+import { ActionSheet } from '@apparatus/blocks-molecules-action-sheet'
+import { ListOption } from '@apparatus/blocks-molecules-list-option'
 import { Overlay } from './src'
 
 export const config: TComponentConfig<TContextParentSize> = {
@@ -19,10 +22,28 @@ export const Component = ({ parentHeight, parentWidth }: TContextParentSize) => 
   <ContextParentSizeProvider parentHeight={parentHeight} parentWidth={parentWidth}>
     <FullCover>
       <Overlay>
-        <Bottom multiplier={10}>
-          <Text level={TEXT_LEVEL_SECONDARY_TITLE}>
-            Demonstration
-          </Text>
+        <Bottom multiplier={0}>
+          <ActionSheet>
+            <Text level={TEXT_LEVEL_SECONDARY_TITLE}>
+              Manage This
+            </Text>
+            <AtomSpacer multiplier={6}/>
+            <ListOption
+              code="edit"
+              optionName="Edit"
+              onPress={() => {}}
+            />
+            <ListOption
+              code="remote"
+              optionName="Remove"
+              onPress={() => {}}
+            />
+            <ListOption
+              code="export-csv"
+              optionName="Export CSV"
+              onPress={() => {}}
+            />
+          </ActionSheet>
         </Bottom>
       </Overlay>
     </FullCover>
